c26317db6865647abc3fe17c366d5f7ca0d030d9
gidung <gidung.id@gmail.com>
2020-03-22 23:13:32 +0700

    “tugas”

---
+++ ClassTaxi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace LatihanClassDanObject
+{
+	public class Taxi
+	{
+		public string DriverName { get; set; }
+		public bool OnDuty = true;
+		public int NumPassenger { get; set; }
+
+		public void TaxiInfo()
+		{
+			Console.WriteLine("Driver Name: {0}", DriverName);
+			Console.Write("On Duty: ");
+			Console.WriteLine(OnDuty ? "Yes" : "No");
+			Console.WriteLine("Num of Passenger: {0}", NumPassenger);
+			
+		}
+
+		public void PickupPassenger()
+		{
+			Console.WriteLine("{0} Sedang Menjemput Penumpang ", DriverName);
+		}
+
+		public void DropOffPassenger()
+		{
+			Console.WriteLine("{0} Selesai Mengantar Penumpang ", DriverName);
+		}
+
+	}
+}
---
+++ ProgramTaxi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace LatihanClassDanObject
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Taxi taxi = new Taxi();
+
+			taxi.DriverName = "Hadi Miftahul Huda";
+			taxi.OnDuty = true;
+			taxi.NumPassenger = 10;
+
+			taxi.TaxiInfo();
+			taxi.PickupPassenger();
+			taxi.DropOffPassenger();
+
+			Console.ReadKey();
+		}
+
+	}
+}
